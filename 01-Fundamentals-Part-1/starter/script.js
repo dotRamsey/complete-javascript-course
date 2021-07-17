39fa653af37603b9713c4c50c9c2869bc8014bc5
dotRamsey <dotramsey87@gmail.com>
2021-07-17 20:31:09 +0200

    Progress

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,20 @@
+// LECTURE: Values and Variables
+
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+
+// Assignment LECTURE: Values and Variables
+
+let country = "Netherlands";
+let continent = "Europe";
+let population = 16000000;
+
+console.log(country, continent, population);
+
+let isIsland = false;
